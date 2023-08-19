9915b2314ca248df22fc8773a315522fca6fdaf7
naomeic <meiyeechin99@gmail.com>
2023-08-19 18:53:02 +1000

    feat: pdf downloader

--- tests/actmemory/download-object-test.py
+++ tests/actmemory/download-object-test.py
@@ -1,17 +1,37 @@
 from bs4 import BeautifulSoup
+import requests
+import time
 
 def get_html_from_url(url):
-    return html_soup
+    # requests library to download html given a url
+    url = url.strip()
+    #print('url in func', url)
+    html = requests.get(url).text
+    #print(html)
+    # parse
+    soup = BeautifulSoup(html)
+    return soup
 
 def get_digital_object_url(soup):
-    return digital_object_url
+    print(soup)
+    obj = soup.find("div", {"class": "digital-object-reference"})
+    print('obj url', obj.a["href"])
+    return obj.a["href"]
 
 def download_object(url, destination):
+    # requests library to download pdf to file
+    filename = url.split('/')[-1]
+    print('filename',filename)
+    r = requests.get(url)
+    open(filename, 'wb').write(r.content)
     return filename
 
-with open("urls.txt", "r") as f:
-    urls = f.read_lines()
-    for url in urls:
+print('starting download')
+with open("tests/actmemory/urls.txt", "r") as f:
+    for url in f.readlines():
+        print('url', url)
         soup = get_html_from_url(url)
         object_url = get_digital_object_url(soup)
-        download_object(object_url, "pdfs")
+        print(object_url)
+        download_object(object_url, "{}/pdfs".format(url.split('/')[-1]))
+        time.sleep(5)
